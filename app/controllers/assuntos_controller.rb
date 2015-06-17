aac8984ca5b7510e6123f9f47976c6e89657df44
grisotto <grisotto18@gmail.com>
2015-06-16 23:35:35 -0300

    Regras para acesso a assuntos e temas

--- app/controllers/assuntos_controller.rb
+++ app/controllers/assuntos_controller.rb
@@ -36,8 +36,9 @@ class AssuntosController < ApplicationController
       if @assunto.save
         #format.html { redirect_to @assunto, notice: 'Assunto was successfully created.' }
         #format.json { render :show, status: :created, location: @assunto }
-        format.html { redirect_to @assunto, notice: 'Assunto was successfully created.' }
-        format.json { render :index, status: :created, location: @assunto }
+     format.html { redirect_to assuntos_url, notice: 'O Assunto foi criado com sucesso.' }
+      format.json { head :no_content }
+
       else
         format.html { render :new }
         format.json { render json: @assunto.errors, status: :unprocessable_entity }
@@ -73,7 +74,7 @@ class AssuntosController < ApplicationController
 
 def admin_only
     unless current_user.admin?
-      redirect_to :back, :alert => "Access denied."
+       redirect_to assuntos_url
     end
   end
     # Use callbacks to share common setup or constraints between actions.
@@ -81,9 +82,11 @@ def admin_only
       @assunto = Assunto.friendly.find(params[:id])
       #@assunto = Assunto.find(params[:id])
       #Logica para nao deixar o usuario editar modulos com disponivel = 0
-     # unless @assunto.disponivel == 1
-      #    redirect_to :back, :alert => "Access denied."
-      # end
+      unless @assunto.disponivel == 1
+   
+         redirect_to assuntos_url, :alert => "Este assunto ainda está em fase de aprovação. Aguarde" 
+      
+       end
 
     end
 
